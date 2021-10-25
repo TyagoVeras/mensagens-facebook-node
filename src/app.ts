c8476dc8eb8e835becbe6f18b73d8ea0dac5610d
Tyago Veras <tyagodesign@gmail.com>
2021-10-25 17:35:41 -0300

    iniciando projeto com facebook

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+import { Server } from "socket.io";
+
+import { router } from "./routes";
+
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+app.get("/facebook", (request, response) => {
+  response.redirect(
+    `https://www.facebook.com/v12.0/dialog/oauth?client_id=1319098141858942`
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
+export { serverHttp, io };
